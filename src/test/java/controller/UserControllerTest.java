7b0fcda66d278276d55903e4923e140a0cb69a99
Piotr Marecki <pmarecki.pro@gmail.com>
2016-06-14 20:46:39 +0200

    reorderings

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("classpath:mdispatcher-servlet.xml")
+@ContextConfiguration("classpath:mdispatcher-servlet-for-test.xml")
 public class UserControllerTest {
 
     @Autowired WebApplicationContext wac;
